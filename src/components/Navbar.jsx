08b3daa72c5e3b9dbd82f6184ada1cf06f84c039
Icaro Jordano <Icarojordano>
2024-09-23 05:17:03 -0300

    feat: adiciona section navbar a pagina com funcao dark mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 import './Navbar.css'
 
+import { FaMoon } from "react-icons/fa";
+import { CiMenuFries } from "react-icons/ci";
+import { GoSun } from "react-icons/go";
+
 export function Navbar() {
+    const [tema,setTema]=useState(false)
+    function mudar() {
+        setTema(!tema)
+    }
     return(
-        <nav className='Navbar'>
+        <nav className={`Navbar ${tema?'dark':"light"}`}>
             <p>{'</Icaro>'}</p>
             <div className="icons">
-                
+                <FaMoon style={{'display':tema?'none':'flex'}} onClick={mudar}></FaMoon>
+                <GoSun style={{'display':tema?'flex':'none'}}  onClick={mudar}>  </GoSun>
+                <CiMenuFries></CiMenuFries>                
             </div>
         </nav>
     )
