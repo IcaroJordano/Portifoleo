823037a5fc22c0d71a2f2f7e188d52ddd34166c4
Icaro Jordano <Icarojordano>
2024-09-23 04:25:07 -0300

    ci: install react icons

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+export function Navbar() {
+    return(
+        <nav className='Navbar'>
+            <p>{'</Icaro>'}</p>
+            <div className="icons">
+                
+            </div>
+        </nav>
+    )
+}
