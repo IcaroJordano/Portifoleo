b8b9dc860be6404017d7652361b5ce4f93ca9c96
Icaro Jordano <Icarojordano>
2024-09-27 03:39:00 -0300

    refactor: componentiza a secao de apresentacao em Apresentacao.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,19 @@
 import "./About.css"
+import { Apresentacao } from "./Apresentacao"
 import { Slide } from "./Slide"
+
+import { BiSpeaker } from "react-icons/bi"
 
 export function About() {
     return(
         <section className="About">
             <Slide></Slide>
-            <h2></h2>
+            <Apresentacao></Apresentacao>
+            <div className="soft_skils">
+                <BiSpeaker></BiSpeaker>
+            </div>
+            <div className="hard_skils"></div>
+            <div className="Metodologias"></div>
         </section>
     )
 }
---
+++ src/components/Apresentacao.jsx
@@ -0,0 +1,11 @@
+import "./Apresentacao.css"
+
+export function Apresentacao() {
+    return(
+        <section className="Apresentacao">
+            <h2> Hello, I'm <span>Icaro Jordano</span></h2>
+            <h3>Desenvolvedor web Front-end e <span>Freelancer</span> Atuando em Fortaleza, Ceará</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur ut laudantium, delectus cumque, qui totam sapiente ducimus animi ullam tempore sint illum eveniet aspernatur tempora corrupti quos atque? Temporibus. <br></br><br></br>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae eveniet suscipit deserunt laboriosam voluptate itaque, deleniti, nihil nostrum consequatur officiis dolore quidem mollitia atque adipisci? Nobis et sapiente optio? Culpa?</p>
+        </section>       
+    )
+}
