da948b3357d100e003a5f610daeda0a915d90849
Icaro Jordano <Icarojordano>
2024-09-27 05:29:23 -0300

    feat: config and style SoftSkills and CardSoftSkills

--- src/components/CardSoftSkills.jsx
+++ src/components/CardSoftSkills.jsx
@@ -1,9 +1,12 @@
-import "./CardSoftSkills"
+import "./CardSoftSkills.css"
 
-export function CardSoftSkills({image}) {
+export function CardSoftSkills({image,color,titulo}) {
+    console.log({color})
     return(
         <div className="card_soft_skills">
-            <img src={image} alt="" />
+            <img src={image} style={{backgroundColor:`${color}`}} alt="" />
+            <h2 >{titulo}</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tempore quibusdam perferendis, dolor delectus recusandae ipsam sint necessitatibus?</p>
         </div>
     )
 }
--- src/components/SoftSkills.jsx
+++ src/components/SoftSkills.jsx
@@ -1,10 +1,16 @@
 import { CardSoftSkills } from "./CardSoftSkills"
 import "./SoftSkills.css"
 
+import oratoria from '../midias/speaking.png'
+
 export function SoftSkils() {
     return(
-        <section className="soft_skils">
-                <CardSoftSkills></CardSoftSkills>
+        <section className="soft_skills">
+            <h2>Soft Skills. <span>______________</span> </h2>
+            <CardSoftSkills image={oratoria} color="#164e63" titulo={"Oratoria"}></CardSoftSkills>
+            <CardSoftSkills image={oratoria} color="#164e63" titulo={"Oratoria"}></CardSoftSkills>
+            <CardSoftSkills image={oratoria} color="#164e63" titulo={"Oratoria"}></CardSoftSkills>
+            <CardSoftSkills image={oratoria} color="#164e63" titulo={"Oratoria"}></CardSoftSkills>
         </section>
     )
 }
