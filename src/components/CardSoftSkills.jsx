84967b3fb2d41e9999d00e2efb4a5fb3d33349e6
Icaro Jordano <Icarojordano>
2024-09-28 02:50:15 -0300

    wip

--- src/components/CardSoftSkills.jsx
+++ src/components/CardSoftSkills.jsx
@@ -5,8 +5,8 @@ export function CardSoftSkills({image,color,titulo}) {
     return(
         <div className="card_soft_skills">
             <img src={image} style={{backgroundColor:`${color}`}} alt="" />
-            <h2 >{titulo}</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tempore quibusdam perferendis, dolor delectus recusandae ipsam sint necessitatibus?</p>
+            <h2>{titulo}</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tempore quibusdam perferendis,</p>
         </div>
     )
 }
--- src/components/SoftSkills.jsx
+++ src/components/SoftSkills.jsx
@@ -6,7 +6,7 @@ import oratoria from '../midias/speaking.png'
 export function SoftSkils() {
     return(
         <section className="soft_skills">
-            <h2>Soft Skills. <span>______________</span> </h2>
+            <h2>Soft Skills. <span className="line"> ______________</span> </h2>
             <CardSoftSkills image={oratoria} color="#164e63" titulo={"Oratoria"}></CardSoftSkills>
             <CardSoftSkills image={oratoria} color="#164e63" titulo={"Oratoria"}></CardSoftSkills>
             <CardSoftSkills image={oratoria} color="#164e63" titulo={"Oratoria"}></CardSoftSkills>
