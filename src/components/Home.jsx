953d58651721bdeb6c876f0ea9cacda6ddbda28c
Icaro Jordano <Icarojordano>
2024-10-03 03:35:21 -0300

    feat: home desktop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,33 +10,36 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 
 export function Home() {
-    return(
+    return (
         <section className="Home" id="Home">
             <div className="image">
                 <img src={avatar} alt="" />
             </div>
-            <div className="apresentacao">
-                <h2>Frontend Devoloper</h2>
-                <h1>Icaro <span>Jordano</span></h1>
-            </div>
-            <div className="logos">
-                <a href="https://www.linkedin.com/in/icaro-jordano/" target="_blank" rel="noopener noreferrer">
-                <FaLinkedinIn></FaLinkedinIn>
-                </a>
-                <a href="https://wa.me/558599683-7880" target="_blank" rel="noopener noreferrer">
-                <FaWhatsapp></FaWhatsapp>
-                </a>
-                <a href="mailto:icarojordano08@gmail.com?" target="_blank" rel="noopener noreferrer">
-                <AiOutlineMail></AiOutlineMail>
-                </a>
-                <a href="https://github.com/IcaroJordano" target="_blank" rel="noopener noreferrer">
-                <FaGithub></FaGithub>
-                </a>
-                
-            </div>
-            <div className="buttons">
-                <button id="button2">Download CV</button>
-                <button>Ver Projetos </button>
+            <div className="textos">
+                <div className="apresentacao">
+                    <h2>Frontend Devoloper</h2>
+                    <h1>Icaro <span>Jordano</span></h1>
+                </div>
+                <div className="logos">
+                    <a href="https://www.linkedin.com/in/icaro-jordano/" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedinIn></FaLinkedinIn>
+                    </a>
+                    <a href="https://wa.me/558599683-7880" target="_blank" rel="noopener noreferrer">
+                        <FaWhatsapp></FaWhatsapp>
+                    </a>
+                    <a href="mailto:icarojordano08@gmail.com?" target="_blank" rel="noopener noreferrer">
+                        <AiOutlineMail></AiOutlineMail>
+                    </a>
+                    <a href="https://github.com/IcaroJordano" target="_blank" rel="noopener noreferrer">
+                        <FaGithub></FaGithub>
+                    </a>
+        
+                </div>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui nemo cumque ea </p>
+                <div className="buttons">
+                        <button id="button2">Download CV</button>
+                        <button>Ver Projetos </button>
+                    </div>
             </div>
         </section>
     )
