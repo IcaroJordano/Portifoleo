d75f3598378a9f68f40924c4226d6d5f39809284
Icaro Jordano <Icarojordano>
2024-10-03 05:42:08 -0300

    feat: card_soft_skills desktop

--- src/components/About/CardSoftSkills.jsx
+++ src/components/About/CardSoftSkills.jsx
@@ -1,12 +1,14 @@
 import "./CardSoftSkills.css"
 
 
-export function CardSoftSkills({image,color,titulo}) {
-    return(
+export function CardSoftSkills({ image, color, titulo }) {
+    return (
         <div className="card_soft_skills">
-            <img src={image} style={{backgroundColor:`${color}`}} alt="" />
+            <img src={image} style={{ backgroundColor: `${color}` }} alt="" />
             <h2>{titulo}</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tempore quibusdam perferendis,</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tempore quibusdam perferendis,
+                <span className="mais" >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur tempore quibusdam perferendis,</span>
+            </p>
         </div>
     )
 }
