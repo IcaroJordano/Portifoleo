1aa84ae1faa4c8b5b27621353994d22ab59c27e2
Icaro Jordano <Icarojordano>
2024-10-05 06:12:28 -0300

    feat: align cards

--- src/CardProjects.jsx
+++ src/CardProjects.jsx
@@ -1,5 +1,4 @@
 import "./CardProjects.css"
-import image from "./midias/image-produto.jpeg"
 
 import { LuArrowUpRight } from "react-icons/lu";
 
@@ -7,23 +6,23 @@ import { LuArrowUpRight } from "react-icons/lu";
 import { AiOutlineLink } from "react-icons/ai";
 
 
-export function CardProjects() {
+export function CardProjects({imagem}) {
     return(
         <div className="card_projects">
-            <img src={'https://miro.medium.com/v2/resize:fit:1400/1*pVxCmIW9kV28plnkC9mpMA.jpeg'} alt="" />
+            <img src={imagem} alt="aa" />
             <h2>Titulo do projeto</h2>
             <div className="tags">
                 <span>React</span>
                 <span>Taiwind</span>
-                <span>Taiwind</span>
+                {/* <span>Taiwind</span> */}
                 {/* <span>Taiwind</span>
                 <span>API</span>
                 <span>Taiwind</span>
                 <span>API</span>
                 <span>API</span> */}
             </div>
-            <a href=""><AiOutlineLink className="deploy"></AiOutlineLink></a>
-            <a href=""><LuArrowUpRight className="ver_mais" ></LuArrowUpRight></a>
+            <a href=""><AiOutlineLink title='ver deploy' className="deploy"></AiOutlineLink></a>
+            <a href=""><LuArrowUpRight title='mais sobre'className="ver_mais" ></LuArrowUpRight></a>
         </div>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import { CardProjects } from "../CardProjects"
 import "./Projects.css"
 
+import image from "../midias/image-produto.jpeg"
 import { BsRocketTakeoff } from "react-icons/bs";
 
 export function Projects() {
@@ -9,10 +10,12 @@ export function Projects() {
             {/* <h2>Projects. <span className="line"> ____span> </h2> */}
             <h2 class="title"><BsRocketTakeoff></BsRocketTakeoff> Projetos</h2>
             <div className="cards">
-            <CardProjects></CardProjects>
-            <CardProjects></CardProjects>
-            <CardProjects></CardProjects>
-            <CardProjects></CardProjects>
+            <CardProjects imagem={image}></CardProjects>
+            <CardProjects imagem={'https://miro.medium.com/v2/resize:fit:1400/1*pVxCmIW9kV28plnkC9mpMA.jpeg'}></CardProjects>
+            <CardProjects imagem={image}></CardProjects>
+            <CardProjects imagem={image}></CardProjects>
+            <CardProjects imagem={image}></CardProjects>
+            <CardProjects imagem={image}></CardProjects>
             </div>
         </section>
     )
